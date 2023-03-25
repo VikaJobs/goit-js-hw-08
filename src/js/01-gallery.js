f0f2ebca81e21f4756a5a6c2e5c188fc0b1d81b8
VikaJobs <rabotilova@gmail.com>
2023-03-25 11:04:45 +0200

    init

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const items = galleryItems
+  .map(item => {
+    return `
+    <a class="gallery_item" href="${item.original}" >
+    <img
+    class="gallery_image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+    />
+    </a>`;
+  })
+  .join('');
+const galleryBox = document.querySelector('.gallery');
+galleryBox.innerHTML = items;
+new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'buttom',
+  swipeClose: true,
+  animationSpeed: 300,
+});
 console.log(galleryItems);
